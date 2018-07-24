bb6e3d897dd84481d7fe186de209584662ab44fc
lldryicell <junkim7093@naver.com>
2018-07-24 12:49:53 +0900

    link map activity

--- app/src/main/java/com/example/user/visionschoollife/MainActivity.java
+++ app/src/main/java/com/example/user/visionschoollife/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.user.visionschoollife;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton imageButton11 = (ImageButton)findViewById(R.id.imageButton11);
+        imageButton11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
